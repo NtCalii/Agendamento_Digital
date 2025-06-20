bba2f9960284662fdcc5f27f5b1750c2b0f3f430
Nathan Caliani <nathan.caliani09@gmail.com>
2025-06-20 19:15:01 -0300

    Remover Railway

--- App.py
+++ App.py
@@ -4,7 +4,6 @@ from Database import db
 from Models import Usuarios, Horarios
 import hashlib
 from datetime import date, time
-import os
 
 app = Flask(__name__)
 app.secret_key = "senha_secreta"
@@ -111,5 +110,4 @@ def home():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
